e395c1feab0ee0ecf63352d58c77f1e62384ab04
nnow991 <nnowak91@gmail.com>
2020-02-24 16:40:04 +0100

    menu_added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/menu';
+import ReactDOM from "react-dom";
 
 function App() {
   return (
     <div className="App">
+<Menu/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 PetCare App        </p>
         
       </header>
+
     </div>
   );
 }
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
+ReactDOM.render(
+  <App>
+    <Menu />
+  </App>,
+  document.getElementById("root")
+);
 
 export default App;
---
+++ my-app/src/components/menu.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Input, Menu } from 'semantic-ui-react'
+
+export default class MenuExampleSecondary extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu secondary>
+        <Menu.Item
+          name='Pacjent'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Leki'
+          active={activeItem === 'messages'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='Kalendarz'
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        />
+          <Menu.Item
+          name='Historia leczenia'
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        />
+          <Menu.Item
+          name='Diagramy'
+          active={activeItem === 'friends'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search...' />
+          </Menu.Item>
+          <Menu.Item
+            name='logout'
+            active={activeItem === 'logout'}
+            onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+    )
+  }
+}
