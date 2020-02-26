208116cbf1adb9ebb12017d0258ecc315cd27bed
nnow991 <nnowak91@gmail.com>
2020-02-26 18:31:10 +0100

    pet_card_edited

--- my-app/src/components/pet-card.js
+++ my-app/src/components/pet-card.js
@@ -3,16 +3,17 @@ import { Button, Header, Image, Modal } from 'semantic-ui-react'
 
 const ModalModalExample = () => (
   <Modal trigger={<Button>Karta pacjenta</Button>}>
-    <Modal.Header>Select a Photo</Modal.Header>
+    <Modal.Header>Imię pacjenta</Modal.Header>
     <Modal.Content image>
       <Image wrapped size='medium' src='https://media.istockphoto.com/photos/fluffy-gray-kitten-of-a-russian-blue-cat-picture-id858701710?k=6&m=858701710&s=612x612&w=0&h=cxMmreqn3-__M9gNKWa8f-fqzzUfjoyPkSSJjUVlanM=' />
       <Modal.Description>
-        <Header>Default Profile Image</Header>
-        <p>
-          We've found the following gravatar image associated with your e-mail
-          address.
-        </p>
-        <p>Is it okay to use this photo?</p>
+        <Header>Nazwa:</Header>
+        <p><em>(pies/kot/ryba)</em></p><br/>
+        <p>Wiek:</p>
+        <p>Waga:</p>
+        <p>Przebyte choroby:</p>
+        <p>Inne ważne informacje:</p>
+        <p>Dane kontaktowe opiekuna:</p>
       </Modal.Description>
     </Modal.Content>
   </Modal>
