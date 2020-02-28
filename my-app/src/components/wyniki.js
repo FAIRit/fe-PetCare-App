e66fbb26a5d474012ef773ab655c1d21a7e1652c
nnow991 <nnowak91@gmail.com>
2020-02-28 17:04:47 +0100

    fixed_warrning

--- my-app/src/components/wyniki.js
+++ my-app/src/components/wyniki.js
@@ -4,7 +4,7 @@ import Link from './Link'
 const ListExampleCelled = () => (
   <div>
 
-  <container class="medicines">
+  <container className="medicines">
   <List celled>
     <List.Item>
       <List.Content>
