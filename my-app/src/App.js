a2cb40b704b72dbfad74a7423a1cea4b7739c91d
nnow991 <nnowak91@gmail.com>
2020-04-01 00:36:15 +0200

    multilanguage_library_added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+
+import React, { Component, Suspense } from 'react';
 import Menu from './components/menu';
 import {
   BrowserRouter as Router,
@@ -14,20 +14,31 @@ import Diagrams from './components/Diagrams/Diagrams'
 import Results from './components/Results/Results'
 import ListaPacjentow from './components/ListaPacjetow/ListaPacjentow'
 import Archive from './components/Docs/archive'
-
-
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
 styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
 document.head.appendChild(styleLink);
-
+ 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Menu />
+  const { t } = useTranslation();
 
+  function handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
+
+  return (
+
+    <div>
+      <div className="language" style ={{width:'100%', padding:'2rem, 0'}}>
+      <button onClick={()=>handleClick('en')}>English</button>
+      <button onClick={()=>handleClick('ge')}>German</button>
+      
+      </div>
+      <Router>
+          <Menu />
           <Switch>
             <Route path="/Leki">
               <Medicines />
@@ -54,10 +65,10 @@ function App() {
             <Archive/>
             </Route>
           </Switch>
-        </div>
       </Router>
+      <div><p>{t('Language.1')}</p></div>
+      </div>
   );
 }
-
 
 export default App;
---
+++ my-app/src/components/Languages/i18next.js
@@ -0,0 +1,49 @@
+
+import i18n from 'i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+const fallbackLng = ['en'];
+const availableLanguages = ['en', 'ge', 'chi'];
+
+
+const options = {
+  order: [ 'navigator', 'htmlTag', 'path', 'subdomain'],
+
+  lookupQuerystring: 'lng',
+  lookupCookie: 'i18next',
+  lookupLocalStorage: 'i18nextLng',
+  lookupFromPathIndex: 0,
+  lookupFromSubdomainIndex: 0,
+
+  caches: ['localStorage', 'cookie'],
+  excludeCacheFor: ['cimode'],
+
+  cookieMinutes: 10,
+  cookieDomain: 'myDomain',
+
+  htmlTag: document.documentElement,
+
+  checkWhitelist: true
+}
+
+i18n
+  .use(Backend) 
+
+  .use(LanguageDetector) 
+
+  .use(initReactI18next) 
+
+  .init({
+    fallbackLng, 
+    debug: true,
+    whitelist: availableLanguages,
+    detection: options,
+
+    interpolation: {
+      escapeValue: false
+    },
+  });
+
+export default i18n;
