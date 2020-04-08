790729cffc4d2ced0179bfa27313a83de1903c58
nnow991 <nnowak91@gmail.com>
2020-04-08 23:20:42 +0200

    transfer_styles_to_index

--- my-app/src/components/Languages/i18next.js
+++ my-app/src/components/Languages/i18next.js
@@ -4,8 +4,8 @@ import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-const fallbackLng = ['en'];
-const availableLanguages = ['en', 'ge', 'pl'];
+const fallbackLng = ['pl'];
+const availableLanguages = ['pl', 'en'];
 
 
 const options = {
